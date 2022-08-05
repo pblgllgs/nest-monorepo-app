7d2b8f5b39b66e7a08fe9c0a2b43408b982a7b52
pblgllgs <pbl.gllgs@gmail.com>
2022-08-04 20:36:23 -0400

    espera los headeres

--- apps/app-1/src/app.controller.ts
+++ apps/app-1/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ResponseDto, SendDto } from './dto/dto';
 
-@Controller()
+@Controller('tbk')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  hello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/getCredentials')
+  getCredentials(
+    @Headers('Tbk-Api-Key-Id') header1: string,
+    @Headers('Tbk-Api-Key-Secret') header2: string,
+    @Body() body: SendDto,
+  ): Promise<ResponseDto> {
+    return this.appService.getCredentials(body, header1, header2);
   }
 }
